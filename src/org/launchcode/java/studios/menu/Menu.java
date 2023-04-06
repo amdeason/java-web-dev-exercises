88f05e0c8cdd2e0dd5f40278f5b5bd4e2b7c50a9
andrew <deasonam@gmail.com>
2023-04-06 16:40:50 -0500

    studio 3

--- src/org/launchcode/java/studios/menu/Menu.java
+++ src/org/launchcode/java/studios/menu/Menu.java
@@ -1,6 +1,24 @@
 package org.launchcode.java.studios.menu;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Menu {
+    private String season;
+    private Date lastUpdated;
+    private ArrayList<MenuItem> menuItems;
+
+    public Menu(String season) {
+        this.season = season;
+    }
+    public Menu(String season, Date lastUpdated) {
+        this.season = season;
+        this.lastUpdated = lastUpdated;
+    }
+    public Menu(String season, Date lastUpdated, ArrayList<MenuItem> menuItems){
+        this.season = season;
+        this.lastUpdated = lastUpdated;
+        this.menuItems = menuItems;
+    }
 
 }
